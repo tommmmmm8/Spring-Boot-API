38ed5a2cf5e97d23f912444875d8a8a3f473135a
Tom <tom8.chr@gmail.com>
2025-04-07 16:38:46 +0200

    Format ManagerResponseDTO and TeamResponseDTO for improved readability

--- football-management-system/src/main/java/com/tom/footballmanagement/DTO/TeamResponseDTO.java
+++ football-management-system/src/main/java/com/tom/footballmanagement/DTO/TeamResponseDTO.java
@@ -1,13 +1,13 @@
 package com.tom.footballmanagement.DTO;
 
-import com.tom.footballmanagement.Entity.Team;
-
 import java.time.LocalDate;
 
-public record TeamResponseDTO(Long team_id,
-                              String name,
-                              LocalDate founded_year,
-                              String stadium,
-                              String manager,
-                              String league) {
+public record TeamResponseDTO(
+        Long team_id,
+        String name,
+        LocalDate founded_year,
+        String stadium,
+        String manager,
+        String league
+) {
 }
